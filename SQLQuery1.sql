24e2cc20ad304452fe3a0521e02faafe0a820b41
EDUARDO PINHEIRO <40127546+eduardopinheiro20@users.noreply.github.com>
2018-09-18 16:14:13 -0300

    Cadastro de cliente

---
+++ SQLQuery1.sql
@@ -0,0 +1,29 @@
+
+use LojaDoces;		
+
+select  * from [dbo].[Cliente];
+
+INSERT INTO dbo.Cliente VALUES
+
+('Eduardo silva', 'M', '1995-05-26', 1),  
+('Daniel Morais','M','1984-01-02', 1),
+('Talita Nascimento','F','1999-12-30', 1),
+('Emerson Gabriel','M','1920-12-30',0), 
+('Lucas Damasceno','M','1930-11-02',1),
+('Leila Martins','F','1975-04-22',0),
+('Letícia Neves','F','1999-12-03',1),
+('Janaína Couto','F','1987-11-12',1),
+('Carlisson Rosa','M','2010-08-01', 1),
+('Jackson Telles','M','1999-01-23', 1),
+('Danilo Araujo','M','1975-12-10', 0),
+('Andreia Delfino','F','1975-07-01', 1),
+('Valter Vilmerson','M','1985-10-12', 1),
+('Allan Silva','M','1993-11-11', 1),
+('Rosana Kunz','F','1935-01-16', 1),
+('Josiane Dutra','F','1950-01-20', 1),
+('Elvis Schwarz','M','2011-05-07', 1),
+('Paulo Narley','M','1997-03-17', 1),
+('Joade Assis','M','1930-12-01',0),
+('Nara Matos','F','1978-03-17', 1);
+
+
